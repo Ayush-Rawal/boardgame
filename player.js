cb874aefed4fc91dc7b81e4426b7977205f454ab
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2020-03-28 13:15:15 +0530

    Add render and pos change functionality to player

--- player.js
+++ player.js
@@ -1,15 +1,19 @@
-import Weapon from './weapon'
+import Weapon from './weapon.js'
 /**
  * Represents a player.
  * @constructor
  */
-function Player() {
+export default function Player(name, x, y) {
+	this.name = name
 	this.hp = 400
 	this.weapon = new Weapon()
 	this.armor = 0
-	this.render = {
+	this.renderData = {
 		currentFrame: 0,
+		frameX: 16,
+		frameY: 16
 	}
+	this.pos = { x: x, y: y }
 }
 
 const PlayerAssetURL = ".\\Assets\\kp1\\characters\\knight_16x16-spritesheet_no-bkg_char-set-1.png"
@@ -17,17 +21,33 @@ const playerSprite = new Image()
 playerSprite.src = PlayerAssetURL
 Player.prototype.sprite = playerSprite
 
-Player.prototype.move = function () {}
+Player.prototype.move = function ({direction, magnitude}) {
+	switch(direction) {
+		case 'DOWN':
+			this.pos.y += magnitude
+			break;
+		case 'RIGHT':
+			this.pos.x += magnitude
+			break;
+		case 'UP':
+			this.pos.y -= magnitude
+			break;
+		case 'LEFT':
+			this.pos.x -= magnitude
+			break;
+		default:
+			throw new Error("Invalid direction")
+	}
+}
 
-Player.prototype.attack = function (player) {} // take player 2 as arg?
+Player.prototype.attack = function (player) {
+	player.hp -= this.weapon.damage * player.armor
+	// TODO: Animate weapon sprite animation
+}
 
-Player.prototype.getAvailableMoves = function () {}
-
-function a () {
-// Down Right Top Left
-// Rows n, n+3 till 12, 15th row empty 16x16
-
-	var x = 0, y = 0
-	const spriteX = 256, spriteY = 256
-	const frameX = 16, frameY = 16
+Player.prototype.render = function (ctx) {
+	let {frameX, frameY} = this.renderData
+	let {x, y} = this.pos
+	// TODO: change 0,0 to set dynamically acc to current frame
+	ctx.drawImage(this.sprite, 0, 0, frameX, frameY, x, y, frameX, frameY)
 }
