0f0b5aa9479515d0c71027febe9df4b0cac3c310
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2020-03-28 12:03:43 +0530

    Add board render function and data

--- board.js
+++ board.js
@@ -5,6 +5,22 @@
  * @param {number} sizeX - Width of the board
  * @param {number} sizeY - Length of the board.
  */
-export default function Board(sizeX = 4, sizeY = 4) {
-	this.board = new Array(sizeY, new Array(sizeX))
+export default function Board(url, bgSize, sizeX = 4, sizeY = 4) {
+	let board = new Array(sizeX).fill(null)
+	this.board = board.map(row => (row = new Array(sizeY).fill(null)))
+	let img = new Image()
+	img.src = url
+	this.bg = img
+	this.bgSize = bgSize
+}
+
+Board.prototype.render = function (ctx) {
+	this.board.forEach((row, i) => {
+		row.forEach((_, j) => {
+			ctx.drawImage(
+				this.bg, 0, 0, this.bgSize, this.bgSize,
+				i * this.bgSize, j * this.bgSize, this.bgSize, this.bgSize
+			)
+		})
+	})
 }
