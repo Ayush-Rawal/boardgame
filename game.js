9e31d91649e7a0bc814a46882affce8e7b947df1
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2020-03-30 16:57:29 +0530

    fix player initialization Players were not being initialized correctly due to empty player array at time of construction

--- game.js
+++ game.js
@@ -2,8 +2,7 @@ import Board from './board'
 import Player from './player'
 
 export default function Game (sizeX, sizeY, numPlayers = 2) {
-	this.board = new Board(sizeX, sizeY)
-	this.players = new Array(numPlayers, new Player())
+	this.players = new Array(numPlayers).fill(null).map((_, i) => new Player(`Player ${i}`, 0,0))
 	this.turn = 0
 	this.totalTurns = 0
 	this.isGameOver = false
