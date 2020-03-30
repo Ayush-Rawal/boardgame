5c3d42869e9aeec21ff389a4ae6a3cbf7991ce2a
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2020-03-30 18:47:31 +0530

    Add weapon object

--- weapon.js
+++ weapon.js
@@ -2,7 +2,29 @@
  * Represents a weapon.
  * @constructor
  * @param (number) damage - damage done per hit
+ * @param (Sprite) sprite - weapon sprite
 */
-export default function Weapon(damage = 40) {
+export default function Weapon(damage = 40, sprite) {
+	this.damage = damage
+	this.sprite = sprite
+}
 
+/**
+ * Animate the weapon sprite.
+ * @param (number) damage - damage done per hit
+ * @param (Sprite) sprite - weapon sprite
+*/
+Weapon.prototype.animate = function() {
+	console.log("Smash Smash!!")
+	this.sprite.animate()
+}
+
+/**
+ * Render the weapon sprite.
+ * @param (object) ctx - 2D canvas context for rendering
+ * @param (number) destX - X coordinate at which to render the sprite
+ * @param (number) destXY - Y coordinate at which to render the sprite
+*/
+Weapon.prototype.render = function (ctx, destX, destY) {
+	this.sprite.render(ctx, destX, destY)
 }
