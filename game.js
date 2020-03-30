3822134203d4e8849aa2a4bc4c3b6c410737770d
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2020-03-30 17:11:29 +0530

    Add tile render info to board initialization

--- game.js
+++ game.js
@@ -2,6 +2,12 @@ import Board from './board'
 import Player from './player'
 
 export default function Game (sizeX, sizeY, numPlayers = 2) {
+	this.board = new Board(
+		"./assets/tiles/200x200/tiles_named_by_description/grass_biome/grass-no_edges.png",
+		200,
+		sizeX,
+		sizeY
+	)
 	this.players = new Array(numPlayers).fill(null).map((_, i) => new Player(`Player ${i}`, 0,0))
 	this.turn = 0
 	this.totalTurns = 0
