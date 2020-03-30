50550eb8a0d9226eaf4fdc1dae86ba5f74421e41
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2020-03-30 17:28:29 +0530

    Add key for player movement

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 import Board from './board.js'
 import Player from './player.js'
+import onKey from './node_modules/keymaster/keymaster.js'
 
 export default function Game (sizeX, sizeY, numPlayers = 2) {
 	this.board = new Board(
@@ -15,6 +16,12 @@ export default function Game (sizeX, sizeY, numPlayers = 2) {
 	this.winner = null
 	this.score = new Array(numPlayers)
 	this.gameTime = 0
+
+	onKey('up, W', () => this.players[0].move('UP', 8))
+	onKey('left, A', () => this.players[0].move('LEFT', 8))
+	onKey('down, S', () => this.players[0].move('DOWN', 8))
+	onKey('right, D', () => this.players[0].move('RIGHT', 8))
+
 }
 
 Game.prototype.render = function (ctx) {
