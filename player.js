02313c6f6c62fc96802110882e65d336b1519a20
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2020-03-30 18:02:11 +0530

    Add player function documentation

--- player.js
+++ player.js
@@ -22,8 +22,13 @@ export default function Player(name, x, y) {
 const PlayerAssetURL = "./assets/kp1/characters/knight_16x16-spritesheet_no-bkg_char-set-1.png"
 const playerSprite = new Image()
 playerSprite.src = PlayerAssetURL
-Player.sprite = playerSprite
+Player.prototype.sprite = playerSprite
 
+/**
+ * Move player on the board
+ * @param (enum) direction - direction to move in. One of "UP", "RIGHT", "DOWN", "LEFT" 
+ * @param (number) magnitude - number of positions to move
+ */
 Player.prototype.move = function (direction, magnitude) {
 	switch(direction) {
 		case 'DOWN':
@@ -43,6 +48,10 @@ Player.prototype.move = function (direction, magnitude) {
 	}
 }
 
+/**
+ * Attack another player
+ * @param (Player) opponent - player to attack 
+ */
 Player.prototype.attack = function (opponent) {
 	let actualDamage = this.weapon.damage * opponent.armor
 	opponent.hp -= actualDamage
@@ -50,12 +59,19 @@ Player.prototype.attack = function (opponent) {
 	opponent.showDamage(actualDamage)
 }
 
+/**
+ * Defend from attacks during turn
+ */
 Player.prototype.defend = function () {
 	// TODO: End extra armor at end of turn
 	// Note: Add a status mechanism that takes care of status effects ending at diff turns
 	this.armor = 0.5
 }
 
+/**
+ * Render Player
+ * @param (object) ctx - 2D canvas context for rendering
+ */
 Player.prototype.render = function (ctx) {
 	let {frameX, frameY} = this.renderData
 	let {x, y} = this.pos
@@ -63,8 +79,14 @@ Player.prototype.render = function (ctx) {
 	ctx.drawImage(this.sprite, 0, 0, frameX, frameY, x, y, frameX, frameY)
 }
 
+/**
+ * Show damage dealt to player
+ * @param (number) damage - damage done 
+ */
 Player.prototype.showDamage = function (damage) {
 	// TODO
 	// Note: show rounded damage to avoid showing things like damaged by 33.333333
 	console.log(`Player damaged by ${damage}`)
 }
+
+(url = "D:/Projects/Boardgame/assets/#2 - Transparent & Drop Shadow.png", srcX = 1, srcY = 5, frameW = 32, frameH = 32)
