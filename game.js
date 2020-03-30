fa9dfd8b6f831e69ca5f273334bedad869e5dac2
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2020-03-30 22:20:42 +0530

    Add key support for movement

--- game.js
+++ game.js
@@ -9,19 +9,19 @@ export default function Game (sizeX, sizeY, numPlayers = 2) {
 		sizeX,
 		sizeY
 	)
-	this.players = new Array(numPlayers).fill(null).map((_, i) => new Player(`Player ${i}`, 0,0))
-	this.turn = 0
-	this.totalTurns = 0
+	this.players = new Array(numPlayers).fill(null).map((_, i) => new Player(`Player ${i}`, 0, 0))
+	this.playerWithTurn = 0
+	this.rounds = 0
 	this.isGameOver = false
 	this.winner = null
 	this.score = new Array(numPlayers)
 	this.gameTime = 0
 
-	onKey('up, W', () => this.players[0].move('UP', 8))
-	onKey('left, A', () => this.players[0].move('LEFT', 8))
-	onKey('down, S', () => this.players[0].move('DOWN', 8))
-	onKey('right, D', () => this.players[0].move('RIGHT', 8))
-
+	onKey('up, W', () => this.players[this.playerWithTurn].move('UP', 8))
+	onKey('left, A', () => this.players[this.playerWithTurn].move('LEFT', 8))
+	onKey('down, S', () => this.players[this.playerWithTurn].move('DOWN', 8))
+	onKey('right, D', () => this.players[this.playerWithTurn].move('RIGHT', 8))
+	onKey('T', () => this.playerWithTurn = (this.playerWithTurn + 1) % numPlayers)
 }
 
 Game.prototype.render = function (ctx) {
