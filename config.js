01abcea0b295114fffbd297eee0011a95519215d
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2020-04-22 04:09:21 +0530

    add config file

---
+++ config.js
@@ -0,0 +1,19 @@
+export default  {
+	board: {
+		sprite: {
+			uri: "./assets/tiles/100x100/tiles_named_by_description/grass_biome/dirt-dual_grass_edges-bottom_end.png",
+			srcX: 0,
+			srcY: 0,
+			frameW: 32,
+			frameH: 32
+		},
+		sizeX: 8,
+		sizeY: 8
+	},
+	players: [{
+		sprite: ""
+	}],
+	weapons: [{
+		sprite: ""
+	}]
+}
