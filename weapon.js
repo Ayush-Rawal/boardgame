3e226e19b10f1a6a00e4cdbc898e27f301858a05
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2020-04-22 04:07:09 +0530

    add dest width and height flexibility to weapons

--- weapon.js
+++ weapon.js
@@ -1,10 +1,10 @@
 /**
  * Represents a weapon.
  * @constructor
- * @param (number) damage - damage done per hit
  * @param (Sprite) sprite - weapon sprite
+ * @param (number) [damage=40] - damage done per hit
 */
-export default function Weapon(damage = 40, sprite) {
+export default function Weapon(sprite, damage = 40) {
 	this.damage = damage
 	this.sprite = sprite
 }
@@ -16,7 +16,7 @@ export default function Weapon(damage = 40, sprite) {
 */
 Weapon.prototype.animate = function() {
 	console.log("Smash Smash!!")
-	this.sprite.animate()
+	this.sprite.animate([])
 }
 
 /**
@@ -25,6 +25,6 @@ Weapon.prototype.animate = function() {
  * @param (number) destX - X coordinate at which to render the sprite
  * @param (number) destXY - Y coordinate at which to render the sprite
 */
-Weapon.prototype.render = function (ctx, destX, destY) {
-	this.sprite.render(ctx, destX, destY)
+Weapon.prototype.render = function (ctx, destX, destY, destW, destH) {
+	this.sprite.render(ctx, destX, destY, destW, destH)
 }
