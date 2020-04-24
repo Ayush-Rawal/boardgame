f00b902ded1667dfbaa9c128efb4c660a0e0a7d6
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2020-04-24 19:11:08 +0530

    add items to config

--- config.js
+++ config.js
@@ -11,9 +11,43 @@ export default  {
 		sizeY: 8
 	},
 	players: [{
-		sprite: ""
+		sprite: {}
 	}],
 	weapons: [{
-		sprite: ""
+		sprite: {
+			uri: "./assets/Shikashi's Fantasy Icons Pack/2 - Transparent & Drop Shadow.png",
+			srcX: 1,
+			srcY: 5,
+			frameW: 32,
+			frameH: 32,
+		},
+		damage: 10,
+		offset: {
+			x: 8,
+			y: -2
+		}
+	}],
+	consumables: [{
+		name: "Elixir",
+		affectedAttribute: "hp",
+		effect: 40,
+		sprite: {
+			uri: "./assets/Shikashi's Fantasy Icons Pack/2 - Transparent & Drop Shadow.png",
+			srcX: 0,
+			srcY: 9,
+			frameW: 32,
+			frameH: 32,
+		}
+	}, {
+		name: "Rock Armor Potion",
+		affectedAttribute: "armor",
+		effect: 0.25,
+		sprite: {
+			uri: "./assets/Shikashi's Fantasy Icons Pack/2 - Transparent & Drop Shadow.png",
+			srcX: 3,
+			srcY: 9,
+			frameW: 32,
+			frameH: 32,
+		}
 	}]
 }
